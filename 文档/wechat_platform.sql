001df43e5e8346ec5667eec95dd08297866a6575
luoqi <136428148@qq.com>
2016-09-03 22:03:24 +0800

    提交sql

---
+++ 文档/wechat_platform.sql
@@ -0,0 +1,30 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : mysql
+Source Server Version : 50172
+Source Host           : localhost:3306
+Source Database       : express
+
+Target Server Type    : MYSQL
+Target Server Version : 50172
+File Encoding         : 65001
+
+Date: 2016-09-03 22:02:47
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for wechat_platform
+-- ----------------------------
+DROP TABLE IF EXISTS `wechat_platform`;
+CREATE TABLE `wechat_platform` (
+  `app_id` varchar(11) DEFAULT NULL,
+  `app_secret` varchar(255) DEFAULT NULL,
+  `access_token` varchar(255) DEFAULT NULL,
+  `jsapi_ticket` varchar(255) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
